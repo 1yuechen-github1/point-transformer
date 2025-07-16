09e15e701fd4b266408001835f250db4894b6fe2
1yuechen-github1 <2207034659@qq.com>
2025-07-16 16:31:05 +0800

    修复__getitem__读label应该为-1

--- util/s3dis.py
+++ util/s3dis.py
@@ -1,5 +1,5 @@
 import os
-from random import random
+import random  # 确保正确导入
 
 import numpy as np
 import SharedArray as SA
@@ -7,6 +7,7 @@ from torch.utils.data import Dataset
 
 from util.data_util import sa_create
 from util.data_util import data_prepare
+import torch
 
 
 class S3DIS(Dataset):
@@ -30,7 +31,7 @@ class S3DIS(Dataset):
     # def __getitem__(self, idx):
     #     data_idx = self.data_idx[idx % len(self.data_idx)]
     #     data = SA.attach("shm://{}".format(self.data_list[data_idx])).copy()
-    #     coord, feat, label = data[:, 0:3], data[:, 3:6], data[:, 6]
+    #     coord, feat, label = data[:, 0:3], data[:, 3:6], data[:, -1]
     #     coord, feat, label = data_prepare(coord, feat, label, self.split, self.voxel_size, self.voxel_max, self.transform, self.shuffle_index)
     #
     #     # 在 __getitem__ 返回前添加检查
@@ -41,12 +42,12 @@ class S3DIS(Dataset):
         while True:  # 循环直到找到符合条件的sample
             data_idx = self.data_idx[idx % len(self.data_idx)]
             data = SA.attach("shm://{}".format(self.data_list[data_idx])).copy()
-            coord, feat, label = data[:, 0:3], data[:, 3:6], data[:, 6]
+            coord, feat, label = data[:, 0:3], data[:, 3:6], data[:, -1]
             coord, feat, label = data_prepare(coord, feat, label, self.split, self.voxel_size, self.voxel_max,
                                               self.transform, self.shuffle_index)
 
-            # 检查是否只包含背景（label=0）
-            if np.all(label == 0):
+            # 检查是否只包含背景（label=0） np.all(label == 0)
+            if torch.all(label == 0):
                 # 随机保留30%的背景samples
                 if random.random() < 0.3:  # 30%概率保留
                     break
